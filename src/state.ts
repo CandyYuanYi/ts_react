9e87e4932ee3c158fc24f3c28d5cde6148327a71
yuanyi010 <yuanyi010@ke.com>
2019-12-27 16:13:19 +0800

    声明

---
+++ src/state.ts
@@ -0,0 +1,19 @@
+/*
+ * Created Date: 2019-12-27
+ * Author: 袁毅
+ * ------
+ * Last Modified: Friday 2019-12-27 12:35:12 pm
+ * Modified By: the developer formerly known as 袁毅 at <yuanyi010ke.com>
+ * ------
+ * HISTORY:
+ */
+/* 
+ * ts基础---声明文件
+*/
+import React from 'react';
+import ReactDOM from "react-dom";
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+
+declare var jquery: (selector: string) => any;
+
