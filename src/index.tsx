d64c9a611383e80deb281adfae0ad12ee2ed155e
yuanyi010 <yuanyi010@ke.com>
2019-12-27 11:55:41 +0800

    [firstcommit]

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,56 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<div />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+function sayHello(person:string) {
+   return 'hello,' + person;
+}
+let user = 'amy';
+console.log(sayHello(user))
+
+//原始数据类型
+let flag: boolean = false;
+let newcreateBoolean: boolean = Boolean(1);
+
+let newcreateSsring: string = String(123);
+let nnewcreateSsring: string = `123${flag}`;
+//其他基本类型同上
+
+
+function add (): void {
+   console.log(nnewcreateSsring)
+}
+add()
+
+let myFavoriteNumber: any = 'seven';
+myFavoriteNumber = 7;
+
+let anyThing: any = 'hello';
+console.log(anyThing.myName);
+
+function reverse(x: number): number;
+function reverse(x: string): string;
+function reverse(x: number | string): number | string | undefined {
+    if (typeof x === 'number') {
+        return Number(x.toString().split('').reverse().join(''));
+    } else if (typeof x === 'string') {
+        return x.split('').reverse().join('');
+    }
+}
+reverse(123);
+
+//类型断言
+
+
 serviceWorker.unregister();
+
+function getLength(something: string | number): number {
+   if ((something as string).length) {
+       return (something as string).length;
+   } else {
+       return something.toString().length;
+   }
+}
